1246f066f2b89fbeb7a51240ab7701ddd6c19508
ericcmilligan <59842926+ericcmilligan@users.noreply.github.com>
2024-02-07 20:30:00 +0000

    Created initial schema for the database and inserted test data into the database to check it works as expected.

---
+++ testData.ts
@@ -0,0 +1,27 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+const example_date = new Date(2024, 1, 26);
+
+//To add example test data insert script
+async function createResponse() {
+  const response = await prisma.response.create({
+    data: {
+      value: "true",
+      data_type: "boolean",
+      assessment_id: 1,
+      question_id: 2
+    },
+  });
+
+  console.log('Created reponse:', response);
+}
+
+createResponse()
+  .catch((error) => {
+    console.error('Error creating reponse:', error);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+});

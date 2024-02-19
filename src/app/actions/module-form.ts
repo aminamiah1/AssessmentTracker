dbab2ddb00a7e733fd997b9b28b0f20e390db20d
Joseph Taylor <taylorj51@cardiff.ac.uk>
2024-02-19 15:06:45 +0000

    Add styles and error handling for edit module page

--- src/app/actions/module-form.ts
+++ src/app/actions/module-form.ts
@@ -1,6 +1,8 @@
 "use server";
 
 import { PrismaClient } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 const prisma = new PrismaClient();
 
@@ -8,6 +10,8 @@ export async function editModuleName(formData: FormData) {
   const moduleName = formData.get("module-name") as string;
   const moduleCodeInit = formData.get("module-code-init") as string;
 
+  if (!moduleName) return;
+
   const moduleId = await prisma.module.findFirst({
     where: {
       module_code: moduleCodeInit,
@@ -19,19 +23,27 @@ export async function editModuleName(formData: FormData) {
 
   if (!moduleId) throw new Error("Could not find module with that code.");
 
-  await prisma.module.update({
-    where: {
-      id: moduleId.id,
-    },
-    data: {
-      module_name: moduleName,
-    },
-  });
+  try {
+    await prisma.module.update({
+      where: {
+        id: moduleId.id,
+      },
+      data: {
+        module_name: moduleName,
+      },
+    });
+  } catch (error) {
+    throw new Error("Database Error: Failed to edit Module Name.");
+  }
+
+  revalidatePath("/admin/module-list");
 }
 
 export async function editModuleCode(formData: FormData) {
-  const moduleCode = (formData.get("module-code") as string) || "";
+  const moduleCode = formData.get("module-code") as string;
   const moduleCodeInit = formData.get("module-code-init") as string;
+
+  if (!moduleCode) return;
 
   const moduleId = await prisma.module.findFirst({
     where: {
@@ -44,12 +56,19 @@ export async function editModuleCode(formData: FormData) {
 
   if (!moduleId) throw new Error("Could not find module with that code.");
 
-  await prisma.module.update({
-    where: {
-      id: moduleId.id,
-    },
-    data: {
-      module_code: moduleCode,
-    },
-  });
+  try {
+    await prisma.module.update({
+      where: {
+        id: moduleId.id,
+      },
+      data: {
+        module_code: moduleCode,
+      },
+    });
+  } catch (error) {
+    throw new Error("Database Error: Failed to edit Module Code.");
+  }
+
+  revalidatePath("/admin/module-list");
+  redirect("/admin/module-list");
 }
--- src/app/admin/module-list/edit/[moduleCode]/page.tsx
+++ src/app/admin/module-list/edit/[moduleCode]/page.tsx
@@ -9,30 +9,67 @@ export default async function EditModule({
 }) {
   return (
     <>
-      <form action={editModuleName}>
-        <input
-          type="hidden"
-          name="module-code-init"
-          value={params.moduleCode}
-        />
-        <label htmlFor="module-name">New Module Name</label>
-        <input type="text" name="module-name" id="module-name" />
-        <button type="submit">
-          <RiSave3Fill />
-        </button>
-      </form>
-      <form action={editModuleCode}>
-        <input
-          type="hidden"
-          name="module-code-init"
-          value={params.moduleCode}
-        />
-        <label htmlFor="module-code">New Module Code</label>
-        <input type="text" name="module-code" id="module-code" />
-        <button type="submit">
-          <RiSave3Fill />
-        </button>
-      </form>
+      <div className="flex flex-col justify-center items-center p-10 w-full min-h-fit h-full">
+        <div className="flex flex-col gap-6 justify-center items-center w-2/5 min-w-fit px-4 py-8 border-2 rounded-xl">
+          <h3 className="text-2xl font-bold text-gray-900">
+            Edit Module {params.moduleCode}
+          </h3>
+          <form action={editModuleName} className="flex flex-col gap-2">
+            <input
+              type="hidden"
+              name="module-code-init"
+              value={params.moduleCode}
+            />
+            <label
+              className="flex justify-start items-center col-span-2 text-gray-900"
+              htmlFor="module-name"
+            >
+              Change Module Name
+            </label>
+            <div className="flex gap-4">
+              <input
+                className="flex justify-center items-center w-64 border focus:outline-none py-2 px-3 rounded-xl text-gray-800"
+                type="text"
+                name="module-name"
+                id="module-name"
+              />
+              <button
+                className="flex justify-start items-center text-2xl text-gray-900"
+                type="submit"
+              >
+                <RiSave3Fill />
+              </button>
+            </div>
+          </form>
+          <form action={editModuleCode} className="flex flex-col gap-2">
+            <input
+              type="hidden"
+              name="module-code-init"
+              value={params.moduleCode}
+            />
+            <label
+              className="flex justify-start items-center col-span-2 text-gray-900"
+              htmlFor="module-code"
+            >
+              Change Module Code
+            </label>
+            <div className="flex gap-4">
+              <input
+                className="flex justify-center items-center border focus:outline-none py-2 px-3 rounded-xl text-gray-800"
+                type="text"
+                name="module-code"
+                id="module-code"
+              />
+              <button
+                className="flex justify-start items-center text-2xl text-gray-900"
+                type="submit"
+              >
+                <RiSave3Fill />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }

c7f4b44d38695e6759bad6bfc7043ac5607eea66
c21040245 <milliganec@cardiff.ac.uk>
2024-02-25 14:55:42 +0000

    Having issues with users not persisting in pipeline database for edit user test so instead enhancing the add user test.

--- cypress/e2e/add-user.cy.ts
+++ cypress/e2e/add-user.cy.ts
@@ -27,10 +27,28 @@ describe("Add User", () => {
   });
   // Add a new user
   it("allows a ps-team member to add a user", () => {
-    // Can add a user
+    //Create a new user for pipeline test
     cy.contains("button", "Create New User").click();
     cy.get('[data-cy="name"]').type("New User");
-    cy.get('[data-cy="email"]').type("newuser@example.com");
+    const timestamp = Date.now();
+    const uniqueEmail = `newuser+${timestamp}@example.com`;
+    cy.get('[data-cy="email"]').clear().type(uniqueEmail);
     cy.get('[data-cy="password"]').type("examplepass");
+    cy.contains("label", "Roles")
+      .next()
+      .find("input")
+      .focus()
+      .type("module_leader{enter}{enter}");
+    cy.contains("button", "Create New User").click({ force: true });
+
+    cy.intercept("POST", "/api/ps-team/create-users", {
+      statusCode: 200,
+      body: {
+        name: "New User",
+        email: uniqueEmail,
+        roles: "module_leader",
+        password: "example",
+      },
+    }).as("addUser");
   });
 });

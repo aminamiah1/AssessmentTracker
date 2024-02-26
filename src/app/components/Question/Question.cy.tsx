4309ce71273626d9ab9fae7303d459c9f0a8763b
c21051023 <wilsonvt@cardiff.ac.uk>
2024-02-26 11:28:16 +0000

    Addded Response component tests for different response types

--- src/app/components/Question/Question.cy.tsx
+++ src/app/components/Question/Question.cy.tsx
@@ -1,32 +1,29 @@
 import { IQuestion } from "@/app/types/form";
 import { Question } from "./Question";
 
-const QUESTION: IQuestion = {
+const QUESTION_TEXT: IQuestion = {
   id: 1,
   prompt: "What is your name?",
   responseType: "text",
 };
 
 describe("<Question />", () => {
-  beforeEach(() => {
-    cy.mount(<Question question={QUESTION} />);
+  it("mounts", () => {
+    cy.mount(<Question question={QUESTION_TEXT} />);
   });
 
-  it("mounts", () => {});
-
   it("should have a class of 'question'", () => {
+    cy.mount(<Question question={QUESTION_TEXT} />);
     cy.get(".question").should("have.class", "question");
   });
 
   it("displays the prompt", () => {
-    cy.contains(QUESTION.prompt).should("be.visible");
+    cy.mount(<Question question={QUESTION_TEXT} />);
+    cy.contains(QUESTION_TEXT.prompt).should("be.visible");
   });
 
   it("displays the response component", () => {
+    cy.mount(<Question question={QUESTION_TEXT} />);
     cy.getByTestId("response").should("be.visible");
-  });
-
-  it("displays the response component as a textarea", () => {
-    cy.getByTestId("response").get("textarea").should("exist");
   });
 });

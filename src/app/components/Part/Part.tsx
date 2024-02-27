ff30001cf8077dd4ebdb4133c109be0d53ece5ed
c21051023 <wilsonvt@cardiff.ac.uk>
2024-02-27 18:50:37 +0000

    - Improved Part component usage (changed props to take a Part object) - Added part fixture for component test

--- src/app/components/Part/Part.tsx
+++ src/app/components/Part/Part.tsx
@@ -1,32 +1,15 @@
-import { IQuestion } from "@/app/types/form";
 import { Question } from "../Question/Question";
 
 interface PartProps {
-  name: string;
+  part: PartWithQuestions;
 }
 
-export function Part({ name }: PartProps) {
-  const questions: IQuestion[] = [
-    {
-      id: 1,
-      prompt: "What is your name?",
-      responseType: "text",
-    },
-    {
-      id: 2,
-      prompt: "Are you a human?",
-      responseType: "boolean",
-    },
-    {
-      id: 3,
-      prompt: "What is your favorite color?",
-      responseType: "multi-choice",
-    },
-  ];
+export function Part({ part }: PartProps) {
+  const { Question: questions } = part;
 
   return (
     <>
-      <h1 style={{ fontSize: 36 }}>{name}</h1>
+      <h1 style={{ fontSize: 36 }}>{part.part_title}</h1>
       <ol>
         {questions.map((question, key) => (
           <Question key={key} question={question} />

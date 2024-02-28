0f286230243a7761c80f69e6af33271de83d3d96
c21051023 <wilsonvt@cardiff.ac.uk>
2024-02-28 14:34:07 +0000

    - Added live and test seed files - Added Tracking Form inputs to live seed file - Added "role" field for each Part model

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,33 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import seedLiveData from "./seeds/live";
+import seedTestData from "./seeds/test";
+
 const prisma = new PrismaClient();
 
+const { NODE_ENV } = process.env;
+
 async function main() {
-  await prisma.part.upsert({
-    where: { id: -1 },
-    update: {},
-    create: {
-      id: -1,
-      part_title: "Test",
-      part_number: 1,
-      Question: {
-        create: [
-          {
-            question_title: "What is your name?",
-            response_type: "string",
-          },
-          {
-            question_title: "Do you use email?",
-            response_type: "boolean",
-          },
-          {
-            question_title: "What's your favourite color?",
-            response_type: "string",
-            choices: ["Red", "Blue", "Green", "Yellow"],
-          },
-        ],
-      },
-    },
-  });
+  if (NODE_ENV === "test" || NODE_ENV === "development") {
+    await seedTestData(prisma);
+  } else if (NODE_ENV === "production") {
+    await seedLiveData(prisma);
+  }
 }
 
 main()

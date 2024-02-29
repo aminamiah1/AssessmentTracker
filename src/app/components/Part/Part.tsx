b001a7b23ce8cfa973978929812b1fe7c835d760
c21051023 <wilsonvt@cardiff.ac.uk>
2024-02-29 12:41:47 +0000

    Added Server Action for the form/part submission

--- src/app/components/Part/Part.tsx
+++ src/app/components/Part/Part.tsx
@@ -7,14 +7,26 @@ interface PartProps {
 export function Part({ part }: PartProps) {
   const { Question: questions } = part;
 
+  async function handleSubmit(form: FormData) {
+    "use server";
+
+    form.forEach((value, questionId) => console.log(questionId, value));
+  }
+
   return (
-    <>
+    <form action={handleSubmit}>
       <h1 className="pt-10 pb-2 text-3xl">{part.part_title}</h1>
       <ol className="flex max-w-prose gap-16 flex-col">
         {questions.map((question, key) => (
           <Question key={key} question={question} />
         ))}
       </ol>
-    </>
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Submit
+      </button>
+    </form>
   );
 }

29ddc10e76579e9cdacb679defa808e4e4b57ad2
c21040245 <milliganec@cardiff.ac.uk>
2024-03-05 12:38:02 +0000

    Added layout file to the module leader assessment management folder to add styling and navbar back.

---
+++ src/app/module-leader/assessment-management/layout.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+import "../../globals.css";
+import Navbar from "../../components/navbar";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Assessment Tracker",
+};
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <Navbar>{children}</Navbar>;
+}

b409576d1e4ab1a359148d96e1e005cdd75eeedf
c21040245 <milliganec@cardiff.ac.uk>
2024-03-12 11:27:59 +0000

    fix pipeline test.

--- cypress/e2e/user-menu.cy.ts
+++ cypress/e2e/user-menu.cy.ts
@@ -13,18 +13,22 @@ describe("User profile menu", () => {
   });
 
   it("Shows profile details for current user", () => {
+    cy.visit("/ps-team/user-management");
     cy.getByTestId("profilePic").click();
     cy.contains("p", "ps@test.net").should("be.visible");
     cy.contains("p", "PS Team User").should("be.visible");
     cy.contains("p", "ps team").should("be.visible");
   });
 
-  it("Shows a message if not logged in", () => {
-    cy.clearAllSessionStorage();
-    cy.clearAllCookies();
-    cy.clearAllLocalStorage();
-    cy.visit("/ps-team/user-management");
-    cy.getByTestId("profilePic").click();
-    cy.contains("p", "Not logged in!").should("be.visible");
-  });
+  // No access to navbar anymore without login so removed test
+
+  // it("Shows a message if not logged in", () => {
+  //   cy.visit("/ps-team/user-management");
+  //   cy.clearAllSessionStorage();
+  //   cy.clearAllCookies();
+  //   cy.clearAllLocalStorage();
+  //   cy.visit("/ps-team/user-management");
+  //   cy.getByTestId("profilePic").click();
+  //   cy.contains("p", "Not logged in!").should("be.visible");
+  // });
 });

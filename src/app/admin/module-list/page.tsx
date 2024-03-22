22da31a815b28b0ae4b30ae3c9a3e2e03168c72a
Joseph Taylor <taylorj51@cardiff.ac.uk>
2024-03-22 17:00:18 +0000

    Create a filter for seeing modules that have been marked as archived

--- src/app/admin/module-list/page.tsx
+++ src/app/admin/module-list/page.tsx
@@ -8,21 +8,22 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { MdArchive, MdEdit } from "react-icons/md";
-import { Users } from "@prisma/client";
+import { FiFilter } from "react-icons/fi";
+import { ModuleStatus } from "@prisma/client";
+import { ModuleData } from "@/app/types/module";
 
 export const dynamic = "force-dynamic";
 
-type ModuleData = {
-  id: number;
-  module_name: string;
-  module_code: string;
-  module_leaders: Users[];
-}[];
-
-async function getModules(searchTerm: string) {
-  const data = await fetch(`/api/module-list/${searchTerm}`, {
-    next: { revalidate: 3600 },
-  });
+async function getModules(
+  searchTerm: string,
+  filters: { active: string; archived: string; completed: string },
+) {
+  const data = await fetch(
+    `/api/module-list/${searchTerm}?active=${filters.active}&archived=${filters.archived}&completed=${filters.completed}`,
+    {
+      next: { revalidate: 3600 },
+    },
+  );
   return data.json();
 }
 
@@ -30,18 +31,27 @@ export default function ModuleList() {
   const { data: session, status } = useSession({ required: true });
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [modules, setModules] = useState<ModuleData>([]);
+  const [activeFilter, setActiveFilter] = useState<boolean>(true);
+  const [archivedFilter, setArchivedFilter] = useState<boolean>(false);
+  const [completedFilter, setCompletedFilter] = useState<boolean>(false);
 
-  const onSearch = (term: string) => {
+  function onSearch(term: string) {
     setSearchTerm(term);
-  };
+  }
+
+  async function fetchModules() {
+    const filters = {
+      active: activeFilter ? "true" : "false",
+      archived: archivedFilter ? "true" : "false",
+      completed: completedFilter ? "true" : "false",
+    };
+    const fetchedModules: ModuleData = await getModules(searchTerm, filters);
+    setModules(fetchedModules);
+  }
 
   useEffect(() => {
-    async function fetchModules() {
-      const fetchedModules: ModuleData = await getModules(searchTerm);
-      setModules(fetchedModules);
-    }
     fetchModules();
-  }, [searchTerm, session]);
+  }, [searchTerm, activeFilter, archivedFilter, completedFilter, session]);
 
   async function handleArchiveModule(moduleCode: string) {
     const res = await archiveModule(moduleCode);
@@ -49,9 +59,26 @@ export default function ModuleList() {
       toast.error(res.error, { position: "bottom-right" });
     } else if (res.success) {
       toast.success(res.success, { position: "bottom-right" });
+    } else {
+      toast.warn("There was an issue when trying to archive the module.", {
+        position: "bottom-right",
+      });
     }
+    fetchModules();
+  }
 
-    setModules(modules.filter(({ module_code }) => module_code !== moduleCode));
+  function handleFiltering(status: ModuleStatus) {
+    switch (status) {
+      case "active":
+        setActiveFilter(!activeFilter);
+        break;
+      case "archived":
+        setArchivedFilter(!archivedFilter);
+        break;
+      case "completed":
+        setCompletedFilter(!completedFilter);
+        break;
+    }
   }
 
   if (status === "loading") {
@@ -76,8 +103,49 @@ export default function ModuleList() {
         </h1>
         {/* Search bar / filtering */}
         <div className="flex flex-col sm:flex-row gap-4 items-center justify-between px-4 py-1">
-          <div data-cy="search-bar">
-            <SearchBar onSearch={onSearch} />
+          <div className="flex items-center gap-4">
+            <div data-cy="search-bar">
+              <SearchBar onSearch={onSearch} />
+            </div>
+            <div className="flex items-center justify-center gap-5 border px-4 py-1 rounded-full">
+              <FiFilter className="text-gray-900 dark:text-gray-100" />
+              <div className="flex justify-center items-center gap-1">
+                <input
+                  data-cy="active-filter"
+                  className="mt-0.5"
+                  onChange={() => handleFiltering(ModuleStatus.active)}
+                  type="checkbox"
+                  name="active"
+                  id="active"
+                  checked={activeFilter}
+                />
+                <label htmlFor="active">Active</label>
+              </div>
+              <div className="flex justify-center items-center gap-1">
+                <input
+                  data-cy="archived-filter"
+                  className="mt-0.5"
+                  onChange={() => handleFiltering(ModuleStatus.archived)}
+                  type="checkbox"
+                  name="archived"
+                  id="archived"
+                  checked={archivedFilter}
+                />
+                <label htmlFor="archived">Archived</label>
+              </div>
+              <div className="flex justify-center items-center gap-1">
+                <input
+                  data-cy="completed-filter"
+                  className="mt-0.5"
+                  onChange={() => handleFiltering(ModuleStatus.completed)}
+                  type="checkbox"
+                  name="completed"
+                  id="completed"
+                  checked={completedFilter}
+                />
+                <label htmlFor="completed">Completed</label>
+              </div>
+            </div>
           </div>
           <div data-cy="create-module-btn">
             <Link
@@ -99,7 +167,8 @@ export default function ModuleList() {
               >
                 <div className="mr-4">
                   <h3 className="text-xl">{module.module_name}</h3>
-                  <p>Module Code: {module.module_code}</p>
+                  {/* Need to put in the academic year here once it's implemented */}
+                  <p>AcaYear-{module.module_code}</p>
                   {module.module_leaders?.length > 0 ? (
                     module.module_leaders.map((module_leader) => (
                       <p> Module Leader: {module_leader.name} </p>
@@ -107,6 +176,11 @@ export default function ModuleList() {
                   ) : (
                     <p>No module leaders assigned.</p>
                   )}
+                  <p
+                    className={`text-sm w-fit mt-2 px-3 pb-0.5 rounded-full text-gray-100 bg-opacity-80 ${module.status === "active" && "bg-green-500"} ${module.status === "archived" && "bg-orange-500"} ${module.status === "completed" && "bg-blue-500"}`}
+                  >
+                    {module.status}
+                  </p>
                 </div>
                 <div className="flex gap-4">
                   <Link
@@ -116,13 +190,15 @@ export default function ModuleList() {
                   >
                     <MdEdit />
                   </Link>
-                  <button
-                    data-cy="archive-button"
-                    className="px-3 py-2 text-2xl border rounded transition-all bg-gray-600 dark:bg-gray-600 text-gray-100 hover:bg-gray-700"
-                    onClick={() => handleArchiveModule(module.module_code)}
-                  >
-                    <MdArchive />
-                  </button>
+                  {module.status === "active" && (
+                    <button
+                      data-cy="archive-button"
+                      className="px-3 py-2 text-2xl border rounded transition-all bg-gray-600 dark:bg-gray-600 text-gray-100 hover:bg-gray-700"
+                      onClick={() => handleArchiveModule(module.module_code)}
+                    >
+                      <MdArchive />
+                    </button>
+                  )}
                 </div>
               </div>
             ))}

79ce7daf466796d7a33639146d5d3289a0f22ba0
c21040245 <milliganec@cardiff.ac.uk>
2024-04-02 10:17:50 +0100

    ps team can now see tracking form for an assessment by clicking associated button on assessment tile.

--- src/app/components/ps-team/AssessmentTilePS.tsx
+++ src/app/components/ps-team/AssessmentTilePS.tsx
@@ -305,6 +305,11 @@ const AssessmentTilePS = ({
               >
                 Delete
               </button>
+              <Link href={`/todo/${assessment.id}`} data-cy="seeTrackingForms">
+                <button className="px-6 mt-2 w-full py-2 text-sm font-medium bg-gray-600 text-white rounded-md hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-700 shadow">
+                  See Tracking Form
+                </button>
+              </Link>
             </div>
           </div>
         </div>

9977c23795c9ff3cf0dccd395fd703ef2e79b6b5
c21040245 <milliganec@cardiff.ac.uk>
2024-04-02 10:04:03 +0100

    fix import csv test 2.

--- cypress/e2e/import-csv.cy.ts
+++ cypress/e2e/import-csv.cy.ts
@@ -46,6 +46,7 @@ describe("Import CSV", () => {
     cy.get(".Toastify__toast-icon", { timeout: 10000 })
       .next()
       .contains(
+        "p",
         "Please select a valid CSV file. Invalid file format.Error parsing csv, check format and try again",
       );
   });
@@ -62,6 +63,7 @@ describe("Import CSV", () => {
     cy.get(".Toastify__toast-icon", { timeout: 10000 })
       .next()
       .contains(
+        "p",
         "Please select a valid CSV file. Invalid file format.Error parsing csv, check format and try again",
       );
   });

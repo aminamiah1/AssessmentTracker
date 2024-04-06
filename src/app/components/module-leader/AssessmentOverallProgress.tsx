6bea695eabec433b493b03133a35253a77e1b68a
c21040245 <milliganec@cardiff.ac.uk>
2024-04-06 11:10:40 +0100

    made text on progress bar mobile responsive

--- src/app/components/module-leader/AssessmentOverallProgress.tsx
+++ src/app/components/module-leader/AssessmentOverallProgress.tsx
@@ -47,28 +47,28 @@ function AssessmentProgressPart1({
           <>
             <div className="flex justify-between">
               <div>
-                <div className="mb-4 text-md text-gray-700 dark:text-white">
+                <div className="mb-4 text-md text-gray-700 dark:text-white max-[1200px]:text-sm  max-[600px]:invisible">
                   <h1 data-cy="trackingStagesComplete">Internal Peer</h1>
                   <h1>Moderation</h1>
                   <h1>Feedback</h1>
                 </div>
               </div>
               <div>
-                <div className="mb-4 text-md text-gray-700 dark:text-white">
+                <div className="mb-4 text-md text-gray-700 dark:text-white max-[1200px]:text-sm max-[600px]:invisible">
                   <h1 data-cy="trackingStagesComplete">Moderation</h1>
                   <h1>Panel</h1>
                   <h1>Comments</h1>
                 </div>
               </div>
               <div>
-                <div className="mb-4 text-md text-gray-700 dark:text-white">
+                <div className="mb-4 text-md text-gray-700 dark:text-white max-[1200px]:text-sm max-[600px]:invisible">
                   <h1 data-cy="trackingStagesComplete">External</h1>
                   <h1>Examiner</h1>
                   <h1>Feedback</h1>
                 </div>
               </div>
               <div>
-                <div className="mb-4 text-md text-gray-700 dark:text-white text-left">
+                <div className="mb-4 text-md text-gray-700 dark:text-white text-left max-[1200px]:text-sm max-[600px]:invisible">
                   <h1 data-cy="trackingStagesComplete">Post</h1>
                   <h1>Marking</h1>
                   <h1>Moderation</h1>
@@ -174,14 +174,14 @@ function AssessmentProgressPart2({
           ) : (
             <div className="flex w-full mb-2">
               <div className="justify-start w-full">
-                <div className="text-md text-gray-700 dark:text-white text-left text-wrap w-2">
+                <div className="text-md text-gray-700 dark:text-white text-left text-wrap w-2 max-[1200px]:text-sm">
                   <h1>Post</h1>
                   <h1>Marking</h1>
                   <h1>Moderation</h1>
                 </div>
               </div>
               <div className="justify-end w-full">
-                <div className="text-md text-gray-700 dark:text-white text-right text-wrap pl-[5em]">
+                <div className="text-md text-gray-700 dark:text-white text-right text-wrap pl-[5em] max-[1200px]:text-sm">
                   <p>Marks And</p>
                   <p>Feedback</p>
                   <p>Avaliability</p>

3f2f851c12451a7fd6dbc3b0b186b323fc62010d
c21040245 <milliganec@cardiff.ac.uk>
2024-04-10 19:36:39 +0100

    get assessment route uses assignees role array instead of assignees.

--- src/app/api/module-leader/assessment/get/route.ts
+++ src/app/api/module-leader/assessment/get/route.ts
@@ -32,14 +32,34 @@ export async function GET(request: NextRequest) {
     const assessment = await prisma.assessment.findUnique({
       where: { id },
       include: {
+        // Get assessment specific role for assignees
+        assigneesRole: {
+          select: {
+            role: true,
+            user: {
+              select: { name: true, email: true },
+            },
+          },
+        },
         module: { select: { module_name: true } },
-        assignees: { select: { id: true, name: true, roles: true } },
       },
     });
 
+    // Restructure assessment into the expected format with assignees
+    const formattedAssessment = {
+      ...assessment,
+      assignees: assessment
+        ? assessment.assigneesRole.map((assigneeRole) => ({
+            name: assigneeRole.user.name,
+            email: assigneeRole.user.email,
+            role: assigneeRole.role,
+          }))
+        : [], // Provide an empty array if assessment is null
+    };
+
     // Check if assessment was found and return details
     if (assessment) {
-      return NextResponse.json(assessment);
+      return NextResponse.json(formattedAssessment);
     } else {
       console.error("Error retrieving assessment");
       return new NextResponse(

5b2427d8a4a643938d6731358349fbe67af10732
c21040245 <milliganec@cardiff.ac.uk>
2024-04-19 11:31:18 +0100

    Removed odd space added onto progress bar section title(external examiner)

--- src/app/components/trackingProgress/logicForBars/AssessmentProgressPart1.tsx
+++ src/app/components/trackingProgress/logicForBars/AssessmentProgressPart1.tsx
@@ -64,7 +64,7 @@ export default function AssessmentProgressPart1({
               <div>
                 <div className="mb-4 text-md text-gray-700 dark:text-white max-[1200px]:text-sm max-[944px]:invisible">
                   <h1 data-cy="trackingStagesComplete">
-                    External <br /> Examiner <br /> Feedback{" "}
+                    External <br /> Examiner <br /> Feedback
                   </h1>
                 </div>
               </div>
